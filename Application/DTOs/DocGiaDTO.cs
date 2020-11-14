8de440c4a95f3fded6ad88a6938d0edb3d15b7a0
= <=>
2020-11-14 11:34:22 +0700

    commit

---
+++ Application/DTOs/DocGiaDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs
+{
+    public class DocGiaDTO
+    {
+
+        public int MaDG { set; get; }
+        public string HoDG { set; get; }
+        public string TenDG { set; get; }
+        public DateTime DoBDG { set; get; }
+        public string EmailDG { set; get; }
+        public string DiaChiDG { set; get; }
+        public DateTime NgayDK { set; get; }
+        public DateTime NgayHetHanDK { set; get; }
+    }
+}
---
+++ Domain/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Repositories
+{
+    public interface IRepository<T> where T : IAggregateRoot
+    {
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
