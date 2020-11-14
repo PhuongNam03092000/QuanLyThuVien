b1f4a976675718125b6c9acab389161599926de3
Trương Đạt Nhân <truongdatnhan@gmail.com>
2020-11-14 15:46:32 +0700

    Fix Sách View

--- QLTV/Areas/Admin/Controllers/SachController.cs
+++ QLTV/Areas/Admin/Controllers/SachController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace QLTV.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class SachController : Controller
     {
         public IActionResult Index()
