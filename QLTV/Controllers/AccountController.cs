d18646eefd99b487c434a0cd7a6e5c443f806d7c
Trương Đạt Nhân <truongdatnhan@gmail.com>
2020-11-14 16:49:28 +0700

    Identity Login + Register Work

--- QLTV/Controllers/AccountController.cs
+++ QLTV/Controllers/AccountController.cs
@@ -9,9 +9,11 @@ namespace QLTV.Controllers
     public class AccountController : Controller
     {
         private readonly SignInManager<AppUser> signInManager;
-        public AccountController(SignInManager<AppUser> signInManager)
+        private readonly UserManager<AppUser> userManager;
+        public AccountController(SignInManager<AppUser> signInManager,UserManager<AppUser> userManager)
         {
-            this.signInManager = signInManager;   
+            this.signInManager = signInManager;
+            this.userManager = userManager;
         }
 
         // GET: LoginController
@@ -22,21 +24,6 @@ namespace QLTV.Controllers
 
         [HttpPost]
         public async Task<IActionResult> Login(LoginRequest loginRequest)
-        {
-            if(ModelState.IsValid)
-            {
-                var result = await signInManager.PasswordSignInAsync(loginRequest.UserName, loginRequest.Password,true,false);
-                if(result.Succeeded)
-                {
-                    return RedirectToAction("index", "admin");
-                }
-                ModelState.AddModelError(string.Empty, "Thông tin đăng nhập không hợp lệ");
-            }
-            return RedirectToAction("index");
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Register(LoginRequest loginRequest)
         {
             if (ModelState.IsValid)
             {
@@ -49,6 +36,35 @@ namespace QLTV.Controllers
             }
             return RedirectToAction("index");
         }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(LoginRequest loginRequest)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new AppUser {UserName=loginRequest.UserName,Email = loginRequest.UserName ,DiaChiNV="123",HoNV="Truong",TenNV="Dat Nhan"};
+                var result = await userManager.CreateAsync(user, loginRequest.Password);
+                ModelState.AddModelError(string.Empty, "Thông tin đăng nhập không hợp lệ");
+                if(result.Succeeded)
+                {
+                    RedirectToAction("index");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+            return View();
+        }
+
+        
 
     }
 }
