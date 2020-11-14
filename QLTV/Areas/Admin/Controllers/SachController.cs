c1d33f0aecd40b7609e812c6ecb93724d646576e
Son Goku <hungdq2000@gmail.com>
2020-11-14 20:32:07 +0700

    Giao diện phần sách ( Từ Trang chủ link qua các chức năng )

--- QLTV/Areas/Admin/Controllers/SachController.cs
+++ QLTV/Areas/Admin/Controllers/SachController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace QLTV.Areas.Admin.Controllers
 {
     [Area("Admin")]
+
     public class SachController : Controller
     {
         public IActionResult Index()
