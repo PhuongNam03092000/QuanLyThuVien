33814eeb55f7700e5ddac09e1166c91bf525e928
Trương Đạt Nhân <truongdatnhan@gmail.com>
2020-11-14 14:59:46 +0700

    Fix Identity Work

---
+++ QLTV/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using QLTV.Services;
+using System.Threading.Tasks;
+
+namespace QLTV.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<AppUser> signInManager;
+        public AccountController(SignInManager<AppUser> signInManager)
+        {
+            this.signInManager = signInManager;   
+        }
+
+        // GET: LoginController
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequest loginRequest)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(loginRequest.UserName, loginRequest.Password,true,false);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("index", "admin");
+                }
+                ModelState.AddModelError(string.Empty, "Thông tin đăng nhập không hợp lệ");
+            }
+            return RedirectToAction("index");
+        }
+
+    }
+}
+   
