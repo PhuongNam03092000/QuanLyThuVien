67cdddb38959a1b21b33ec146eebf306d3289b15
= <=>
2020-11-17 05:14:26 +0700

    commit

---
+++ Application/Services/NXBService.cs
@@ -0,0 +1,36 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Services
+{
+    public class NXBService : INXBService
+    {
+        public void CreateNXB(NhaXuatBanDTO nxb)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteNXB(int MaNXB)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<NhaXuatBanDTO> GetDSNXB()
+        {
+            throw new NotImplementedException();
+        }
+
+        public NhaXuatBanDTO GetNXB(int MaNXB)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateNXB(NhaXuatBanDTO nxb)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
