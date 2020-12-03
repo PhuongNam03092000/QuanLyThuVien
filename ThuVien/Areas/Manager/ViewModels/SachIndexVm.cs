2c04440182261fce10cbc7b58cf3224e7d978be2
lannhi02 <phamtrinhlannhi02@gmail.com>
2020-12-04 00:31:31 +0700

    .

--- ThuVien/Areas/Manager/ViewModels/SachIndexVm.cs
+++ ThuVien/Areas/Manager/ViewModels/SachIndexVm.cs
@@ -1,4 +1,6 @@
 ﻿using Application.DTOs;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +12,7 @@ namespace ThuVien.Areas.Manager.ViewModels
     public class SachIndexVm
     {
         public PaginatedList<SachDTO> Sachs { get; set; }
-        public SachDTO sachDto { get; set; }
+        public SachDTO sach { get; set; }
         public string SearchString { get; set; }
         public string SortOrder { get; set; }
     }
