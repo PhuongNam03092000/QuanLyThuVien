1e2e9f8aca91efa3adcbf1f73ecf23e344707235
lannhi02 <phamtrinhlannhi02@gmail.com>
2020-12-16 17:29:43 +0700

    .

--- ThuVien/Areas/Manager/Controllers/HomeController.cs
+++ ThuVien/Areas/Manager/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ThuVien.Areas.Manager.Controllers
 {
     [Area("Manager")]
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ISachRepository sachRepository;
